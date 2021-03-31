4ff35da26d1a6a98f8d72339ea11c51fd426ab04
MaxRybetsky <stsdewar@ya.ru>
2021-04-01 00:01:09 +0300

    Add Entity and DTO of locations.

---
+++ entity/src/main/java/org/example/hrsample/LocationsEntity.java
@@ -0,0 +1,10 @@
+package org.example.hrsample;
+
+import lombok.Data;
+
+@Data
+public class LocationsEntity {
+    private Integer locationId;
+    private String streetAddress;
+    private String city;
+}
