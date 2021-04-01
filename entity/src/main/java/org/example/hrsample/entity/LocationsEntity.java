d8da17ab40e0b7cc4b66f7cb699ab12d6550a9cf
MaxRybetsky <stsdewar@ya.ru>
2021-04-01 16:20:52 +0300

    Add toString, equals and hashCode methods to entity and dto of locations.

--- entity/src/main/java/org/example/hrsample/entity/LocationsEntity.java
+++ entity/src/main/java/org/example/hrsample/entity/LocationsEntity.java
@@ -1,8 +1,12 @@
 package org.example.hrsample.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
+@EqualsAndHashCode
+@ToString
 public class LocationsEntity {
     private Integer locationId;
     private String streetAddress;
