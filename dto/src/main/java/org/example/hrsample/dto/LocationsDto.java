99b5f42e984cd915219af7ca501b6c47859cbd2a
MaxRybetsky <stsdewar@ya.ru>
2021-04-01 18:52:46 +0300

    Add OpenAPI documentation for locations.

--- dto/src/main/java/org/example/hrsample/dto/LocationsDto.java
+++ dto/src/main/java/org/example/hrsample/dto/LocationsDto.java
@@ -1,5 +1,6 @@
 package org.example.hrsample.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -10,8 +11,14 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode
 @ToString
+@Schema(title = "Location data model")
 public class LocationsDto {
+    @Schema(title = "Location's ID", accessMode = Schema.AccessMode.READ_ONLY)
     private Integer locationId;
+
+    @Schema(title = "Street address of location")
     private String streetAddress;
+
+    @Schema(title = "City of location", required = true)
     private String city;
 }
