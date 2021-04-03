736463f860398fcf9fd8ec77d2e743bb0698a2c6
MaxRybetsky <stsdewar@ya.ru>
2021-04-03 21:11:16 +0300

    Fix annotations duplicates in Locations.

--- dto/src/main/java/org/example/hrsample/dto/LocationsDto.java
+++ dto/src/main/java/org/example/hrsample/dto/LocationsDto.java
@@ -2,15 +2,11 @@ package org.example.hrsample.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 /**
  * Location's Model.
  */
 @Data
-@EqualsAndHashCode
-@ToString
 @Schema(title = "Location data model")
 public class LocationsDto {
     @Schema(title = "Location's ID", accessMode = Schema.AccessMode.READ_ONLY)
--- entity/src/main/java/org/example/hrsample/entity/LocationsEntity.java
+++ entity/src/main/java/org/example/hrsample/entity/LocationsEntity.java
@@ -1,12 +1,8 @@
 package org.example.hrsample.entity;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@EqualsAndHashCode
-@ToString
 public class LocationsEntity {
     private Integer locationId;
     private String streetAddress;
