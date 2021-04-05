78a076454295bf80c12469f9fe759aee3a5a24e3
MaxRybetsky <stsdewar@ya.ru>
2021-04-05 20:56:02 +0300

    Update Entities.

--- entity/src/main/java/org/example/hrsample/entity/DepartmentEntity.java
+++ entity/src/main/java/org/example/hrsample/entity/DepartmentEntity.java
@@ -1,15 +1,11 @@
 package org.example.hrsample.entity;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@EqualsAndHashCode
-@ToString
 public class DepartmentEntity {
     private Integer departmentId;
     private String departmentName;
-    private Integer managerId;
-    private Integer locationId;
+    private EmployeeEntity manager;
+    private LocationsEntity location;
 }
--- entity/src/main/java/org/example/hrsample/entity/JobEntity.java
+++ entity/src/main/java/org/example/hrsample/entity/JobEntity.java
@@ -1,14 +1,10 @@
 package org.example.hrsample.entity;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@EqualsAndHashCode
-@ToString
 public class JobEntity {
-    private Integer jobsId;
+    private String jobsId;
     private String jobsTitle;
     private Integer minSalary;
     private Integer maxSalary;
