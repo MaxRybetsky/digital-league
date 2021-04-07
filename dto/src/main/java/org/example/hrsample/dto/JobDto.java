ed5b64157ee54ab937c9cf4b3262b74ca757bfd9
MaxRybetsky <stsdewar@ya.ru>
2021-04-07 16:43:29 +0300

    Add OpenAPI.

--- dto/src/main/java/org/example/hrsample/dto/JobDto.java
+++ dto/src/main/java/org/example/hrsample/dto/JobDto.java
@@ -1,14 +1,20 @@
 package org.example.hrsample.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
  * Job's model.
  */
 @Data
+@Schema(title = "Job data model")
 public class JobDto {
+    @Schema(title = "Job ID")
     private String jobsId;
+    @Schema(title = "Title")
     private String jobsTitle;
+    @Schema(title = "Min Salary")
     private Integer minSalary;
+    @Schema(title = "Max Salary")
     private Integer maxSalary;
 }
