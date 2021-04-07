4cf0402758b2bc4d38dcc29bdc39c3c56d8c047d
MaxRybetsky <stsdewar@ya.ru>
2021-04-07 13:28:51 +0300

    Update Employee Mapper layer.

--- dao/src/main/java/org/example/hrsample/dao/EmployeesMapper.java
+++ dao/src/main/java/org/example/hrsample/dao/EmployeesMapper.java
@@ -1,13 +1,26 @@
 package org.example.hrsample.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.example.hrsample.entity.EmployeeEntity;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 @Repository
 public interface EmployeesMapper {
     Optional<EmployeeEntity> getEmployeeById(Integer id);
+
+    void saveEmployee(EmployeeEntity employeeEntity);
+
+    void updateEmployee(EmployeeEntity employeeEntity);
+
+    List<EmployeeEntity> getEmployeesByParameters(@Param("firstName") String firstName,
+                                                  @Param("lastName") String lastName,
+                                                  @Param("email") String email,
+                                                  @Param("startDate") LocalDate startDate,
+                                                  @Param("endDate") LocalDate endDate);
 }
