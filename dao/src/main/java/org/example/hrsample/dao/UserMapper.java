37521c321bb5a594151cbb894985066fc6031ebc
MaxRybetsky <stsdewar@ya.ru>
2021-04-10 16:58:55 +0300

    Update user mapper.

--- dao/src/main/java/org/example/hrsample/dao/UserMapper.java
+++ dao/src/main/java/org/example/hrsample/dao/UserMapper.java
@@ -1,10 +1,13 @@
 package org.example.hrsample.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.example.hrsample.entity.UserEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Mapper
 @Repository
 public interface UserMapper {
-
+    Optional<UserEntity> getByLogin(String login);
 }
