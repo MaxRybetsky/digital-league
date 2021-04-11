7771613d92f38fe543ca2470b8c8c390c663f31f
MaxRybetsky <stsdewar@ya.ru>
2021-04-11 16:36:34 +0300

    Add Jwt provider and JwtAuthenticationException.

---
+++ core/src/main/java/org/example/hrsample/exception/JwtAuthenticationException.java
@@ -0,0 +1,18 @@
+package org.example.hrsample.exception;
+
+import org.springframework.http.HttpStatus;
+
+import javax.naming.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    private HttpStatus httpStatus;
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+
+    public JwtAuthenticationException(String msg, HttpStatus httpStatus) {
+        super(msg);
+        this.httpStatus = httpStatus;
+    }
+}
