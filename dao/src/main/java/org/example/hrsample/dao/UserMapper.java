135798d975143d1386135b944003ca4d92c55905
MaxRybetsky <stsdewar@ya.ru>
2021-04-12 21:42:49 +0300

    Fix bug with mapper.

--- dao/src/main/java/org/example/hrsample/dao/UserMapper.java
+++ dao/src/main/java/org/example/hrsample/dao/UserMapper.java
@@ -1,10 +1,13 @@
 package org.example.hrsample.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.example.hrsample.entity.UserEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Mapper
 @Repository
